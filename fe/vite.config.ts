e5d76b1d6dc237c2691e8b96eaa5359b9e2d2f21
6kitty <sixeunseoth@gmail.com>
2026-07-06 15:55:13 +0900

    [CHORE] vite config 수정

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -4,9 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    port: 18000,
+    allowedHosts: [
+      "quant-agent.kro.kr",
+    ],
     proxy: {
       "/ai-api": {
-        target: "http://127.0.0.1:8000",
+        target: "http://127.0.0.1:18001",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/ai-api/, ""),
       },
