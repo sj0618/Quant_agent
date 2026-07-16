a6cfcffd47999353c7e1838878a61cc078abf0b5
6kitty <sixeunseoth@gmail.com>
2026-07-16 13:55:32 +0900

    [CHORE] ssl 인증서

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 18000,
     allowedHosts: [
-      "quant-agent.kro.kr",
+      "qt-agent.kro.kr",
     ],
     proxy: {
       "/ai-api": {
